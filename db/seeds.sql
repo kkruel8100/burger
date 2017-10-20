e9789cc6a2bd0fb6eed7e62cb4f0d5d56aeab832
kkruel8100 <kkruel.kruel1@gmail.com>
2017-10-20 16:30:48 -0700

    database setup

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+SELECT * FROM burgers;
+
+INSERT INTO burgers (burger_name) VALUES ("WhataBurger Patty Melt");
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Spicy Sriracha Burger");
