96d3af433d4f894be44ee37f763345e2e9363ab0
kkruel8100 <kkruel.kruel1@gmail.com>
2017-10-22 20:25:44 -0700

    working all and css styling

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require("path");
 
 // Sets up the Express App
 var app = express();
-var PORT = 3000;
+var PORT = 8080;
 
 // Middleware to link to static pages
 app.use(express.static(path.join(__dirname, "/public")));
@@ -14,9 +14,16 @@ app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// ROUTER
-// require("./app/routing/apiRoutes")(app);
-// require("./app/routing/htmlRoutes")(app);
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
 
 // Starts the server to begin listening
 app.listen(process.env.PORT || PORT, function() {
